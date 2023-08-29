0a4f844acb531b90a4c90167900d13b067927a4a
PatricioIgnacio <patricioignacio.santibanezgonz@gmail.com>
2023-08-29 13:44:44 -0400

    Integración de estructura HTML

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,3 +1,25 @@
 export default function HeroSection() {
-  return <h1>HeroSection</h1>;
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hola, soy Patricio Santibáñez</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">Full Stack</span>
+            {""}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <br /> Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
+          <button className="btn btn-primary">Button</button>
+        </div>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
 }
